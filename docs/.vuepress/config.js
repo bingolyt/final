1f84134efcb183bed2b15ed3b56ba22ff6ac5b55
bingolyt <1664461612@qq.com>
2020-06-18 22:45:39 +0800

    lyt云应用专周

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,82 @@
+module.exports = {
+  "title": "小林妙妙屋",
+  "description": "小林同学的心情book",
+  "dest": "public",
+  "head": [
+    [
+      "link",
+      {
+        "rel": "icon",
+        "href": "/favicon.ico"
+      }
+    ],
+    [
+      "meta",
+      {
+        "name": "viewport",
+        "content": "width=device-width,initial-scale=1,user-scalable=no"
+      }
+    ]
+  ],
+  "theme": "reco",
+  "themeConfig": {
+    "nav": [
+      {
+        "text": "Home",
+        "link": "/",
+        "icon": "reco-home"
+      },
+      {
+        "text": "TimeLine",
+        "link": "/timeline/",
+        "icon": "reco-date"
+      },
+      {
+        "text": "Contact",
+        "icon": "reco-message",
+        "items": [
+          {
+            "text": "GitLab",
+            "link": "https://gitlab.com/bobyuan/",
+            "icon": "reco-gitlab"
+          }
+        ]
+      }
+    ],
+    "type": "blog",
+    "blogConfig": {
+      "category": {
+        "location": 2,
+        "text": "Category"
+      },
+      "tag": {
+        "location": 3,
+        "text": "Tag"
+      }
+    },
+    "friendLink": [
+      {
+        "title": "my dance",
+        "desc": "《你最最最重要》呀！！！",
+        "link": "https://www.bilibili.com/video/BV1x54y1B7d5"
+      },
+      {
+        "title": "金刚芭比",
+        "desc": "Hold on to your love.",
+        "link": "https://www.bilibili.com/video/BV1H4411y75o"
+      }
+    ],
+	"logo": "/logo_trans.png",
+    "search": true,
+    "searchMaxSuggestions": 10,
+    "sidebar": "auto",
+    "lastUpdated": "Last Updated",
+    "author": "Lin yuting",
+    "authorAvatar": "/avatar.png",
+    //"record": "xxxx",
+    "startYear": "2020"
+  },
+  "markdown": {
+    "lineNumbers": false
+  }
+}
